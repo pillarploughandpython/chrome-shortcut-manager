787e0d7c7c792933a018b47a5c268c5c0b581100
Jian-Long Huang <jianlong@ntu.edu.tw>
2013-02-21 12:56:20 +0800

    Add README and settings

---
+++ README.md
@@ -0,0 +1,23 @@
+### Introduction
+
+Shortcut Manager is a Chrome extension for customizing shortcut keys. Forked from [here](https://chrome.google.com/webstore/detail/shortcut-manager/mgjjeipcdnnjhgodgjpfkffcejoljijf).
+
+### Shortcuts
+
+Here is my common used shortcuts. You can import the contents in `shortcuts.js`.
+
+* `Alt-l`: Select the right tab
+
+* `Alt-j`: Select the left tab
+
+* `Alt-a`: Select the 1-th tab
+
+* `Alt-e`: Select the last tab
+
+* `Alt-b`: Back
+
+* `Alt-f`: Forward
+
+* `Alt-i`: Scroll to the top
+
+* `Alt-k`: Scroll to the bottom
---
+++ shortcuts.js
@@ -0,0 +1,71 @@
+// ==UserScript==
+// @ShortcutManager
+// @name Select the right tab
+// @namespace 8Jry7K7XIw2H
+// @key Alt+l
+// @include *
+// @execute SelectRightTab([])
+// ==/UserScript==
+
+// ==UserScript==
+// @ShortcutManager
+// @name Select the left tab
+// @namespace 8Jry7K7XIw2H
+// @key Alt+j
+// @include *
+// @execute SelectLeftTab([])
+// ==/UserScript==
+
+// ==UserScript==
+// @ShortcutManager
+// @name Back
+// @namespace 8Jry7K7XIw2H
+// @key Alt+b
+// @include *
+// @execute Back([])
+// ==/UserScript==
+
+// ==UserScript==
+// @ShortcutManager
+// @name Forward
+// @namespace 8Jry7K7XIw2H
+// @key Alt+f
+// @include *
+// @execute Forward([])
+// ==/UserScript==
+
+// ==UserScript==
+// @ShortcutManager
+// @name Select the 1-th tab
+// @namespace 8Jry7K7XIw2H
+// @key Alt+a
+// @include *
+// @execute SelectNthTab(["1"])
+// ==/UserScript==
+
+// ==UserScript==
+// @ShortcutManager
+// @name Select the last tab
+// @namespace 8Jry7K7XIw2H
+// @key Alt+e
+// @include *
+// @execute SelectLastTab([])
+// ==/UserScript==
+
+// ==UserScript==
+// @ShortcutManager
+// @name Scroll to the top
+// @namespace 8Jry7K7XIw2H
+// @key Alt+i
+// @include *
+// @execute ScrollToTop([])
+// ==/UserScript==
+
+// ==UserScript==
+// @ShortcutManager
+// @name Scroll to the bottom
+// @namespace 8Jry7K7XIw2H
+// @key Alt+k
+// @include *
+// @execute ScrollToBottom([])
+// ==/UserScript==
